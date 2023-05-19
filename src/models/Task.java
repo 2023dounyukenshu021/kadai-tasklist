026a850b0f262af030054c76fcef73e431ba00fd
azusa.oowaki <2023dounyukenshu.021@gmail.com>
2023-05-19 10:04:37 +0900

    getAllMessagesを追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,22 +7,30 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllTasks",
+            query = "SELECT m from Task AS m ORDER BY m.id DESC"
+            )
+})
 @Table(name = "tasks")
 public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
-    
+
     @Column(name ="updated_at", nullable = false)
     private Timestamp upadated_at;
-    
+
     @Column(name ="content", nullable = false, length = 255)
     private String content;
 
@@ -57,6 +65,6 @@ public class Task {
     public void setContent(String content) {
         this.content = content;
     }
-    
-    
+
+
 }
